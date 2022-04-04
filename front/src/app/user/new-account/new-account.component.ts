2db704add66328f3ccbc7e069d460a92b5cd415f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-04 10:57:35 +0200

    dev: added new account screen

--- front/src/app/user/new-account/new-account.component.ts
+++ front/src/app/user/new-account/new-account.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.scss']
+  styleUrls: ['./new-account.component.scss'],
 })
 export class NewAccountComponent implements OnInit {
+  f = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    displayName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  submit() {}
 }
