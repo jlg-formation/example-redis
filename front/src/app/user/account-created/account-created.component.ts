c9289e6868b82e7fd28a17a4bbd15675dbf3b382
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-04 11:59:06 +0200

    dev: added html for account-created

--- front/src/app/user/account-created/account-created.component.ts
+++ front/src/app/user/account-created/account-created.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@jlguenego/angular-tools';
 
 @Component({
   selector: 'app-account-created',
   templateUrl: './account-created.component.html',
-  styleUrls: ['./account-created.component.scss']
+  styleUrls: ['./account-created.component.scss'],
 })
 export class AccountCreatedComponent implements OnInit {
+  user: User = {
+    displayName: 'Jean-Louis GUENEGO',
+    email: 'jlguenego@gmail.com',
+    identityProvider: 'example-redis-website',
+    id: 'jlguenego',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
