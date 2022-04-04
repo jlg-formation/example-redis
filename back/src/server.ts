0b101d8005ef6857abff401202635f9748db2a1c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-04 12:26:01 +0200

    dev: started to develop back

---
+++ back/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import serveIndex from "serve-index";
+import { api } from "./api";
+
+const port = +process.env.PORT || 3000;
+const wwwDir = "../front/dist/front";
+
+const app = express();
+
+app.use("/api", api);
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
