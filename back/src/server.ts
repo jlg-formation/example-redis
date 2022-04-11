151451b92012735ed41b0a2dca8ac375ad3ce86c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-11 08:50:32 +0200

    dev: move pubsub

--- back/src/server.ts
+++ back/src/server.ts
@@ -3,9 +3,9 @@ import { createServer } from "http";
 import morgan from "morgan";
 import serveIndex from "serve-index";
 import { api } from "./api.router";
-import { redisClient, redisSession } from "./redis-session";
-import { webSocket } from "./websocket";
 import { pubsub } from "./pubsub";
+import { redisSession } from "./redis-session";
+import { webSocket } from "./websocket";
 
 const port = +process.env.PORT || 3000;
 const wwwDir = "../front/dist/front";
@@ -19,6 +19,7 @@ const wss = webSocket(server);
 app.use(morgan("tiny"));
 
 app.use(redisSession);
+pubsub(wss);
 
 app.use("/api", api(wss));
 
@@ -28,4 +29,3 @@ app.use(serveIndex(wwwDir, { icons: true }));
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-pubsub(wss);
