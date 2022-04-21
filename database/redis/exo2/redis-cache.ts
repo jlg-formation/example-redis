95c765210d24118ab2f618745d0febbd12cc090f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-21 18:15:44 +0200

    dev: housekeeping

--- database/redis/exo2/redis-cache.ts
+++ database/redis/exo2/redis-cache.ts
@@ -1,27 +1,32 @@
 import axios from "axios";
 import { createClient } from "redis";
 
-const client = createClient({
-  url: "redis://localhost:6379",
+const clientRO = createClient({
+  url: process.env.REDIS_RO_URL ?? "redis://localhost:6379",
+});
+
+const clientWO = createClient({
+  url: process.env.REDIS_WO_URL ?? "redis://localhost:6379",
 });
 
 (async () => {
   try {
-    client.on("error", (err) => console.log("Redis Client Error", err));
-
-    await client.connect();
+    clientRO.on("error", (err) => console.log("Redis Client Error", err));
+    await clientRO.connect();
+    clientWO.on("error", (err) => console.log("Redis Client Error", err));
+    await clientWO.connect();
   } catch (err) {
     console.log("err: ", err);
   }
 })();
 
 export const redisAxios = async (url) => {
-  const cache = await client.get(url);
+  const cache = await clientRO.get(url);
   if (cache === null) {
     console.log("Cache MISS");
     const response = await axios(url);
     const data = response.data;
-    await client.set(url, JSON.stringify(data), {
+    await clientWO.set(url, JSON.stringify(data), {
       EX: 30,
     });
     return data;
